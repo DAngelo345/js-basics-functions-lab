4132f110dd0156d6d184cf8f96f077dcaffd0337
DAngelo Thomas <dangelo1thomas@gmail.com>
2021-08-03 22:47:12 -0400

    commit tree 71cf55e73b28479aac9c95dc9de57b7bc5e7ba0a

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(blockNumber) {
+
+    if (blockNumber > 42) {
+        return blockNumber - 42
+    }
+    else {
+        return 42 - blockNumber
+    }  
+}
+
+function distanceFromHqInFeet(blockNumber) {
+    return distanceFromHqInBlocks(blockNumber) * 264;
+}
